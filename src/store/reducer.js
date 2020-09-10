9d6d709e20feea01bab006190174a1d99fd62e0d
Yixiu1996 <jczcycy@163.com>
2020-09-10 21:37:39 +0800

    feat: 更改package.json配置的eject命令

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2020-09-09 22:42:31
- * @LastEditTime: 2020-09-09 23:02:24
+ * @LastEditTime: 2020-09-10 21:21:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \person-app\src\store\reducer.js
  */
+// 默认数据
 const defaultState = {
   style: 'recreation'  // 默认是娱乐模式
-} // 默认数据
+} 
 export default (state = defaultState,action)=>{
   if(action.type === 'changeStyle'){
     let newState = JSON.parse(JSON.stringify(state))
